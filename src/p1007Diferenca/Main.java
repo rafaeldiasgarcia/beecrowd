9b7379f564ec97924b4b9e3892d9bc04aba53ad4
rafaeldiasgarcia <rafdgar@gmail.com>
2026-01-22 01:33:41 -0300

    1007

---
+++ src/p1007Diferenca/Main.java
@@ -0,0 +1,22 @@
+package p1007Diferenca;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int diferenca = a * b - c * d;
+
+        System.out.println("DIFERENCA = " + diferenca);
+
+    }
+ 
+}
