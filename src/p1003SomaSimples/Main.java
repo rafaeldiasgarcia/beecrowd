3bba72ec6c2b0c1ca09516d30c707ab91923589c
rafaeldiasgarcia <rafdgar@gmail.com>
2026-01-22 01:12:55 -0300

    1003

---
+++ src/p1003SomaSimples/Main.java
@@ -0,0 +1,20 @@
+package p1003SomaSimples;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+         
+         Scanner sc = new Scanner(System.in);
+         
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         int soma = a + b;
+         
+         System.out.println("SOMA = " + soma);
+ 
+    }
+ 
+}
